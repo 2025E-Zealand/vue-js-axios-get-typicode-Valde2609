1302b5340b5255f4500fc43ba8c251b427029ef8
Valde2609 <valde2609@gmail.com>
2025-11-12 11:50:30 +0100

    testet lidt axios funktionalitet

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
+const baseUri = 'http://jsonplaceholder.typicode.com/todos/'
 const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            posts:[],
         }
     },
     methods: {
-        myMethod(){
-
+        getData(){
+            axios.get(baseUri)
+            .then(response => {
+                this.posts = response.data
+                console.log(this.posts)
+            })
+            .catch(error => {
+                console.error("There was an error"), error;
+            })
         },
     },
     computed: {
